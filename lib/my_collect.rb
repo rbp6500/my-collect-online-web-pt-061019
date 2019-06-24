d5e920a6ce5bae09d7eadf2bc91b33981e3dba25
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-24 03:42:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,3 +2,7 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
 end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+end
